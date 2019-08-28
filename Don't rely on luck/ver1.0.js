fc4c5f7f8a4d5c619df4c13f349fbf3c45eebb54
newbi462 <isthisemailfree@gmail.com>
2019-08-28 18:17:39 -0400

    first commit

---
+++ Don't rely on luck/ver1.0.js
@@ -0,0 +1,10 @@
+var guess = 42;// if going to hack it might as well make it the awnser to everything
+
+var Math = {
+	random: function(){
+		return 0;// new Math.random()
+	},
+	floor: function(){
+ 		return 42;// no luck when you force the awnser, new Math.floor()
+	},
+}
