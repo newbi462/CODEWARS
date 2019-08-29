e3d8f7c958595e855aab9283b3579f236fbcde5e
newbi462 <isthisemailfree@gmail.com>
2019-08-29 12:59:22 -0400

    Solution

--- Don't rely on luck/ver1.0.js
+++ Don't rely on luck/ver1.0.js
@@ -1,10 +1,10 @@
-var guess = 42;// if going to hack it might as well make it the answer to everything
+var guess = 42;
 
 var Math = {
 	random: function(){
-		return 0;// new Math.random()
+		return 0;
 	},
 	floor: function(){
- 		return 42;// no luck when you force the awnser, new Math.floor()
+ 		return 42;
 	},
 }
