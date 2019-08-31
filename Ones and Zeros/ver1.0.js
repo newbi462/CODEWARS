7144ca409957911d2a6c22eabb0ac7a318d5fdd2
newbi462 <isthisemailfree@gmail.com>
2019-08-31 13:27:16 -0400

    Solution

---
+++ Ones and Zeros/ver1.0.js
@@ -0,0 +1,4 @@
+const binaryArrayToNumber = arr => {
+   var a = arr.join('');
+   var b = parseInt(a,2);
+return b; };
---
+++ Ones and Zeros/ver1.1.js
@@ -0,0 +1,1 @@
+const binaryArrayToNumber = arr => {  return parseInt(arr.join(''), 2);  };
