ebc0bb203c9e2394e759266d4aeb3e442b92f044
newbi462 <isthisemailfree@gmail.com>
2019-09-09 10:57:30 -0400

    IN PROGRESS

--- Multi Line Task++: Hello World/ver1.0.js
+++ Multi Line Task++: Hello World/ver1.0.js
@@ -1,20 +1,27 @@
 f=_=>//coding and coding.. shorter and more shorter..
 
 //call funtion f with out going over 2 charters
-f=()=>"Hello, world!"
-
 f=
 (
 
 )=>//3 not 2
-"Hello, world!"
-
 // f === string "Hello, world!"
-f=()=>
-"H
-ello, world!"
-
 //break apart string in to 2 charater or less segments
-
+"\
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!"// [output] is "Hello, world!", but arrow funtion call makes this over 2 charaters
+//?? is there a .proto funtion that will type this to funtion with our changing the value?
 
 //re asemple string
+/*no need for this as it is as needed*/
