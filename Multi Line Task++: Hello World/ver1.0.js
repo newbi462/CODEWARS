61b7ecae6dec85ee48a97924f49c2c7726b15cd6
newbi462 <isthisemailfree@gmail.com>
2019-09-09 12:31:26 -0400

    IN PROGRESS

--- Multi Line Task++: Hello World/ver1.0.js
+++ Multi Line Task++: Hello World/ver1.0.js
@@ -1,5 +1,3 @@
-f=_=>//coding and coding.. shorter and more shorter..
-
 //call funtion f with out going over 2 charters
 f=
 //_=>//3 not 2, replaced with ['trim']in funtion for type
@@ -8,11 +6,23 @@ f=
 ""
 //?? is there a .proto funtion that will type this to funtion with our changing the value?
 //.trim();// leaves typeof === string
-['trim'] // forces typeof === funtion, but f(); wont run String.prototype.trim called on null or undefined
+[
+'\
+t\
+r\
+i\
+m\
+'] // forces typeof === funtion, but f(); wont run String.prototype.trim called on null or undefined
 //?? is there a .proto that will force this operation to a new funtion it will run?
 // The bind() method creates a new function, function.bind(thisArg, so will "typeof funtion" let this new funtion run?
 //.bind("Hello, world!") // perserves typeof === funtion under the new funtion and forces the ['trim'] funtion to run.
-['bind'] // change sytnatx to make \ compatible
+[
+'\
+b\
+i\
+n\
+d\
+']
 `\
 H\
 e\
