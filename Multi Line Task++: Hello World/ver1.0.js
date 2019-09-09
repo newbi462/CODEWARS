458fc68edac38d790d9f334ea1a8cfd5c80ca3fb
newbi462 <isthisemailfree@gmail.com>
2019-09-09 12:17:18 -0400

    IN PROGRESS

--- Multi Line Task++: Hello World/ver1.0.js
+++ Multi Line Task++: Hello World/ver1.0.js
@@ -2,10 +2,11 @@ f=_=>//coding and coding.. shorter and more shorter..
 
 //call funtion f with out going over 2 charters
 f=
-_=>//3 not 2, replaced with ['trim']in funtion for type
+//_=>//3 not 2, replaced with ['trim']in funtion for type
 // f === string "Hello, world!"
 //break apart string in to 2 charater or less segments
-"\
+""
+/*"\
 H\
 e\
 l\
@@ -18,11 +19,13 @@ o\
 r\
 l\
 d\
-!"// [output] is "Hello, world!", but arrow funtion call makes this over 2 charaters
+!"*/  // [output] is "Hello, world!", but arrow funtion call makes this over 2 charaters
 //?? is there a .proto funtion that will type this to funtion with our changing the value?
 //.trim();// leaves typeof === string
 ['trim'] // forces typeof === funtion, but f(); wont run String.prototype.trim called on null or undefined
-//?? is there a .proto that will force this operation to a new funtiona it will run?
+//?? is there a .proto that will force this operation to a new funtion it will run?
+// The bind() method creates a new function, function.bind(thisArg, so will "typeof funtion" let this new funtion run?
+.bind("Hello, world!") // perserves typeof === funtion under the new funtion and forces the ['trim'] funtion to run. 
 
 //re asemple string
 /*no need for this as it is as needed*/
