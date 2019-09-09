a3cd82e36d5767f3aa354372bb51f65799e451b3
newbi462 <isthisemailfree@gmail.com>
2019-09-09 10:17:16 -0400

    IN PROGRESS

---
+++ Multi Line Task++: Hello World/ver1.0.js
@@ -0,0 +1,13 @@
+f=_=>//coding and coding.. shorter and more shorter..
+
+//call funtion f with out going over 2 charters
+f = function() {
+  return "Hello, world!";
+}
+
+f=()=>"Hello, world!"
+
+
+// f === string "Hello, world!"
+//break aopart string in to 2 charater or less segments
+//re asemple string
