1a3470150c67117797b1b910ceff455651816cd7
newbi462 <isthisemailfree@gmail.com>
2019-09-09 12:39:29 -0400

    CLEAN UP

--- Multi Line Task++: Hello World/ver1.0.js
+++ Multi Line Task++: Hello World/ver1.0.js
@@ -1,21 +1,12 @@
-//call funtion f with out going over 2 charters
 f=
-//_=>//3 not 2, replaced with ['trim']in funtion for type
-// f === string "Hello, world!"
-//break apart string in to 2 charater or less segments
 ""
-//?? is there a .proto funtion that will type this to funtion with our changing the value?
-//.trim();// leaves typeof === string
 [
 '\
 t\
 r\
 i\
 m\
-'] // forces typeof === funtion, but f(); wont run String.prototype.trim called on null or undefined
-//?? is there a .proto that will force this operation to a new funtion it will run?
-// The bind() method creates a new function, function.bind(thisArg, so will "typeof funtion" let this new funtion run?
-//.bind("Hello, world!") // perserves typeof === funtion under the new funtion and forces the ['trim'] funtion to run.
+']
 [
 '\
 b\
@@ -38,7 +29,4 @@ l\
 d\
 !`
 
-//re asemple string
-/*no need for this as it is as needed*/
-
-//console.log(typeof f);
+// acording to the kata, It's possible to complete this in ```28``` lines only., so sould lok for a way to make this 2 line shorter.
