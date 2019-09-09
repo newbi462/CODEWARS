f3f7e6db97282b6913db67c2ca0294609369a8b2
newbi462 <isthisemailfree@gmail.com>
2019-09-09 11:46:33 -0400

    IN PROGRESS

--- Multi Line Task++: Hello World/ver1.0.js
+++ Multi Line Task++: Hello World/ver1.0.js
@@ -2,9 +2,7 @@ f=_=>//coding and coding.. shorter and more shorter..
 
 //call funtion f with out going over 2 charters
 f=
-(
-
-)=>//3 not 2
+_=>//3 not 2, replaced with ['trim']in funtion for type
 // f === string "Hello, world!"
 //break apart string in to 2 charater or less segments
 "\
@@ -22,6 +20,11 @@ l\
 d\
 !"// [output] is "Hello, world!", but arrow funtion call makes this over 2 charaters
 //?? is there a .proto funtion that will type this to funtion with our changing the value?
+//.trim();// leaves typeof === string
+['trim'] // forces typeof === funtion, but f(); wont run String.prototype.trim called on null or undefined
+//?? is there a .proto that will force this operation to a new funtiona it will run?
 
 //re asemple string
 /*no need for this as it is as needed*/
+
+//console.log(typeof f);
