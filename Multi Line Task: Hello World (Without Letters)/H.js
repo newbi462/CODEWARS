bfa8bbc00c5c7b5d8a11a78ce96e89eed108fd67
newbi462 <isthisemailfree@gmail.com>
2019-09-16 20:40:18 -0400

    "H" but after "w" think this is going to end up to long :(

---
+++ Multi Line Task: Hello World (Without Letters)/H.js
@@ -0,0 +1,19 @@
+//"H" === []['filter']['constructor']('return unescape')()([]['filter']['constructor']('return escape')()(([]+[])['italics']()[+[]])[+[]]+(48)+[])
+[]
+['filter']
+['constructor']
+('return unescape')
+()([]
+['filter']
+['constructor']
+('return escape')
+()
+(([]+[])
+['italics']
+()
+[+[]])[+[]]
++
+(48)
++
+[])
+//below here stays
