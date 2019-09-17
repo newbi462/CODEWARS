4213047ad86dd3e62a1d29424e38b7f3d390af75
newbi462 <isthisemailfree@gmail.com>
2019-09-17 12:12:16 -0400

    Need to split up to 1 Charter per line

--- Multi Line Task∞: Hello World/ver1.0.js
+++ Multi Line Task∞: Hello World/ver1.0.js
@@ -1,17 +1,2 @@
-f=
-`
-`
-[
-'\
-t\
-r\
-i\
-m'
-][
-'\
-b\
-i\
-n\
-d'
-]
-("Hello, world")// in vert string and `` to make 1 charter easyer?
+[,Y, ,o, ,u, ,c, ,H, ,e, ,a, ,t, ,d, ,O, ,n, ,T, ,h, ,i, ,s,] = ` t r i m b n d H e l o ,   w ! `
+f = ""[Y+o+u+c][H+u+e+a](t+d+O+O+n+T+h+i+n+o+O+a+s)
