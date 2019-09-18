e15417daec59b228526043eb1349609123aeae3f
newbi462 <isthisemailfree@gmail.com>
2019-09-18 12:58:48 -0400

    Fix my "awnser"

--- Is my friend cheating?/ver1.0.js
+++ Is my friend cheating?/ver1.0.js
@@ -1,14 +1,14 @@
 function removeNb (n) {
-  var sumalln = (n*(n+1))/2; // math to replace loop
+  var sumalln = (n*(n+1))/2;
   var b;
   var a;
-  var awnser = [];
+  var answer = [];
 
   for (let i = n; i > 0; i--) {
     b = i;
     a = (sumalln - b)/(b + 1);
 
-    if (a < n && a % 1 === 0) {awnser.push([a,b]);}
+    if (a < n && a % 1 === 0) {answer.push([a,b]);}
   }
-  return awnser;
+  return answer;
 }
