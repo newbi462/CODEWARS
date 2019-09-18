bcb366d47c5260a49bca89b636838c8bf2f74e62
newbi462 <isthisemailfree@gmail.com>
2019-09-18 13:01:30 -0400

    b is not neaded

--- Is my friend cheating?/ver1.0.js
+++ Is my friend cheating?/ver1.0.js
@@ -1,14 +1,11 @@
 function removeNb (n) {
   var sumalln = (n*(n+1))/2;
-  var b;
   var a;
   var answer = [];
 
   for (let i = n; i > 0; i--) {
-    b = i;
-    a = (sumalln - b)/(b + 1);
-
-    if (a < n && a % 1 === 0) {answer.push([a,b]);}
+    a = (sumalln - i)/(i + 1);
+    if (a < n && a % 1 === 0) {answer.push([a,i]);}
   }
   return answer;
-}
+} // b is not neaded
