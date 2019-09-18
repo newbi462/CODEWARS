cca726d14e3a947fbdafba45195970018ea017c2
newbi462 <isthisemailfree@gmail.com>
2019-09-18 12:45:39 -0400

    Solution

--- Is my friend cheating?/ver1.0.js
+++ Is my friend cheating?/ver1.0.js
@@ -1,48 +1,14 @@
 function removeNb (n) {
-  var arrayof1ton = [];
-  var copyn = n;
-  var a = 0;
-  var b = 0;
-  var prodab = 0;
-  var sumof = 0;
-  var arrayofa = [];
-  var arrayofb = [];
-  var answer = [];
-  var sumall = (n*(n+1))/2;
+  var sumalln = (n*(n+1))/2; // math to replace loop
+  var b;
+  var a;
+  var awnser = [];
 
-  for (let i = 0; i < n; i++) {
-    arrayof1ton.push(copyn);
-    //sumall = sumall + copyn; //= (n*(n+1))/2
-    copyn = copyn - 1;
+  for (let i = n; i > 0; i--) {
+    b = i;
+    a = (sumalln - b)/(b + 1);
+
+    if (a < n && a % 1 === 0) {awnser.push([a,b]);}
   }
-
-  for (let i = 0; i < arrayof1ton.length; i++) {
-    a = arrayof1ton[i];
-    for (let x = 1; x < arrayof1ton.length; x++) {
-      b = arrayof1ton[x];
-      prodab = a * b;
-      sumof = sumall - a - b;
-
-      if (prodab === sumof) {
-        arrayofa.push(a);
-        arrayofb.push(b);
-      }
-      sumof = 0;
-    }
-  }
-
-  answer.push(arrayofa.reverse());
-  answer.push(arrayofb.reverse());
-  if (arrayofa.length > 0) {return answer;}
-  else {
-    answer = [];
-    return answer;
-  }
+  return awnser;
 }
-
-// algebra to replace most loops with math that finds the other var
-/*(a * b) = sum - a - b
-  (a * b) + a = sum - b
-  (a * b) + (a * 1) = sum - b
-  a(b + 1) = sum - b
-  a = (sum - b)/(b + 1)*/
